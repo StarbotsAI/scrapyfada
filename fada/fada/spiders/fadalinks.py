12b01355ccf795a691aa1e4a10af442d31cbe1d8
StarbotsAI <felipe@starbots.com.br>
2024-11-20 04:17:16 -0300

    função de scrap de link 100% finalizada

--- fada/fada/spiders/fadalinks.py
+++ fada/fada/spiders/fadalinks.py
@@ -4,9 +4,7 @@ class FadavendaSpider(scrapy.Spider):
     name = "fadalinks"
     allowed_domains = ["fadaimoveis1.com.br"]
     start_urls = [
-        "https://fadaimoveis1.com.br/busca/?finalidade=Venda&pagina=1",
-        "https://fadaimoveis1.com.br/busca/?finalidade=Aluguel&pagina=1",
-        "https://fadaimoveis1.com.br/busca/?finalidade=Temporada&pagina=1"
+        "https://fadaimoveis1.com.br/busca/?finalidade=Venda+e+Aluguel&pagina=1"
         ]
 
     def parse(self, response):
